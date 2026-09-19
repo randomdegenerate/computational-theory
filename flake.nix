3b47e458142848cd369aedf50a8bbfaa2cd67286
randomdegenerate <sandilkosgahakumbura@gmail.com>
2026-09-19 08:19:29 +0100

    correctly packaged python modules within the actual python package rather than seperately within the dev shells packages

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
 {
   description = "Computational Theory Dev Environment";
 
+  # references used to write this flake
+  #https://discourse.nixos.org/t/how-to-create-flake-nix-for-nix-shell-loading-a-few-packages/58456/2
+  #https://nixos.wiki/wiki/Packaging/Python
+
   inputs = {
     #Package url source
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -22,47 +26,52 @@
         devShells.default = pkgs.mkShellNoCC {
           #Defining the packages active in the dev shell
           packages =
-          with pkgs.python314Packages;
-          [
-            pkgs.python3
-            #python3 packages
+            [
+                # define python3 package and the modules within the package
+                # aligning versioning and ensuring it works
+                (pkgs.python3.withPackages(pythonPackages: with pythonPackages; [
+                    # Interactive Python shell.
+                    ipython
+                    # Jupyter notebook/lab support.
+                    ipykernel
+                    # Jupyter lab.
+                    jupyterlab
+                    # Jupyter notebook.
+                    notebook
+                    # Numerical arrays.
+                    numpy
+                    # Data Frames.
+                    pandas
+                    # Scientific computing.
+                    scipy
+                    # Statistical modeling.
+                    statsmodels
+                    # Download financial market data.
+                    yfinance
+                    # Plotting.
+                    matplotlib
+                    # Statistical data visualization.
+                    seaborn
+                    # Quantum computing.
+                    # qiskit[visualization]
+                    # Quantum simulators.
+                    # qiskit-aer
+                    # Machine learning.
+                    scikit-learn
+                    # Symbolic mathematics.
+                    sympy
+                    # Testing.
+                    pytest
+                ]))
+            ];
 
-            # Interactive Python shell.
-            ipython
-            # Jupyter notebook/lab support.
-            ipykernel
-            # Jupyter lab.
-            jupyterlab
-            # Jupyter notebook.
-            notebook
-            # Numerical arrays.
-            numpy
-            # Data Frames.
-            pandas
-            # Scientific computing.
-            scipy
-            # Statistical modeling.
-            statsmodels
-            # Download financial market data.
-            yfinance
-            # Plotting.
-            matplotlib
-            # Statistical data visualization.
-            seaborn
-            # Quantum computing.
-            # qiskit[visualization]
-            # Quantum simulators.
-            # qiskit-aer
-            # Machine learning.
-            scikit-learn
-            # Symbolic mathematics.
-            sympy
-            # Testing.
-            pytest
-          ];
+            env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+                pkgs.stdenv.cc.cc.lib
+                pkgs.libz
+            ];
 
           #Shell hook runs when shell is entered
-          shellHook = "jupyter notebook";
+          # shellHook = "";
         };
     }
     );
